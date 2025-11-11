9f33e2be7af5e5c325bcde0e18a1367e1d07676b
Erzana Beqaj <erzana.beqaj@student.uni-pr.edu>
2025-11-11 20:12:55 +0100

    Fillimi i file client_redonly.py

---
+++ client_redonly.py
@@ -0,0 +1,9 @@
+import socket
+
+SERVER_IP = "192.168.1.100"
+SERVER_PORT = 5555
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = (SERVER_IP, SERVER_PORT)
+
+print("Klient READ-ONLY u lidh me serverin.")
+print("Komandat: /list, /read, /search, exit")
