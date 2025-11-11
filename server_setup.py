4b98cc92fa73574512d6a8d7831bde745fe0bba7
AnitaCacaj <162963163+nitacacaj3@users.noreply.github.com>
2025-11-11 19:35:34 +0100

    Vazhdim ne server_setup.py

--- server_setup.py
+++ server_setup.py
@@ -6,3 +6,7 @@ SERVER_IP = "0.0.0.0"
 SERVER_PORT = 5555
 MAX_CLIENTS = 4
 TIMEOUT = 60
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((SERVER_IP, SERVER_PORT))
+print(f"Serveri u startua në {SERVER_IP}:{SERVER_PORT}")
